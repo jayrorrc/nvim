1d0876b48c8f0855a5130d295ed9aee71939ceb5
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-16 18:52:51 -0300

    feat: add none-ls

--- lua/plugins/file-management.lua
+++ lua/plugins/file-management.lua
@@ -1,16 +1,15 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim",
-    },
-    config = function ()
-      vim.keymap.set('n', '<leader>ft', ":Neotree filesystem reveal left<CR>", {})
-      vim.keymap.set('n', '<leader>fo', ":Neotree buffers<CR>", {})
-      vim.keymap.set('n', '<leader>fs', ":Neotree git_status<CR>", {})
-    end
-  }
-
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>ft", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>fo", ":Neotree buffers<CR>", {})
+		vim.keymap.set("n", "<leader>fs", ":Neotree git_status<CR>", {})
+	end,
+}
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,10 @@
-return { 
-  "rebelot/kanagawa.nvim", 
-  lazy = false, 
-  name="kanagawa", 
-  priority=1000, 
-  config = function () 
-    require("kanagawa").setup()
-    vim.cmd.colorscheme "kanagawa"
-  end 
+return {
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	name = "kanagawa",
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup()
+		vim.cmd.colorscheme("kanagawa")
+	end,
 }
