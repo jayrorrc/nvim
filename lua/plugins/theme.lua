dea5b4f72a6125000b471ba80c7adc36bd028308
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-17 09:58:05 -0300

    feat: setup a dashboard

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,42 @@
 return {
-	"rebelot/kanagawa.nvim",
-	lazy = false,
-	name = "kanagawa",
-	priority = 1000,
-	config = function()
-		require("kanagawa").setup()
-		vim.cmd.colorscheme("kanagawa")
-	end,
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		name = "kanagawa",
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup()
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{
+		"goolord/alpha-nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.startify")
+
+			dashboard.section.header.val = {
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                     ]],
+				[[       ████ ██████           █████      ██                     ]],
+				[[      ███████████             █████                             ]],
+				[[      █████████ ███████████████████ ███   ███████████   ]],
+				[[     █████████  ███    █████████████ █████ ██████████████   ]],
+				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+			}
+
+			alpha.setup(dashboard.opts)
+		end,
+	},
 }
