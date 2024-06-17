764120ce5d2350001cf3e54a66c7c63d6fe256b7
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-17 10:09:23 -0300

    feat: add keymaps to tab navigation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,3 +2,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.keymap.set("n", "<leader>tp", ":tabn<CR>", {})
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", {})
