d076e2266fbc113a93c82c5517cb4404a3a8039a
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-21 11:13:05 -0300

    feat: add code test

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,3 +13,5 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", {})
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", {})
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", {})
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", {})
+
+vim.keymap.set("n", "<F3>", ":set hlsearch!<CR>", {})
