13bf9223b2be6d0b4613f42f7746ccda304d59d3
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-21 10:51:00 -0300

    feat: add tmux compatibilite

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,3 +8,8 @@ vim.cmd("set relativenumber")
 
 vim.keymap.set("n", "<leader>tp", ":tabn<CR>", {})
 vim.keymap.set("n", "<leader>tp", ":tabp<CR>", {})
+
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", {})
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", {})
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", {})
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", {})
