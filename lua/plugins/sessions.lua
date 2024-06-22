1797d489cae0016c4ebcf38c924842c7bde4ac63
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-22 10:25:17 -0300

    ref: desable sessions plugin

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,15 +1,16 @@
 return {
-  -- "rmagatti/auto-session",
-  -- config = function()
-  --  require("auto-session").setup({
-  --    log_level = "error",
-  --    auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-  --
-  --    cwd_change_handling = {
-  --      post_cwd_changed_hook = function()
-  --        require("lualine").refresh()
-  --      end,
-  --    },
-  --  })
-  -- end,
+  "rmagatti/auto-session",
+  enabled = false,
+  config = function()
+    require("auto-session").setup({
+      log_level = "error",
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+
+      cwd_change_handling = {
+        post_cwd_changed_hook = function()
+          require("lualine").refresh()
+        end,
+      },
+    })
+  end,
 }
