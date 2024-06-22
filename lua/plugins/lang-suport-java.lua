ff72a670adc6d20def8d163ca5cc2749d2449391
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-22 10:36:41 -0300

    fix: nvim-java dependencies

--- lua/plugins/lang-suport-java.lua
+++ lua/plugins/lang-suport-java.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-java/nvim-java",
-    enabled = false,
+    --enabled = false,
     dependencies = {
       "nvim-java/lua-async-await",
       "nvim-java/nvim-java-refactor",
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,57 +1,66 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+    --config = function()
+    --	require("mason").setup()
+    --end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     opts = {
       auto_install = true,
     },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "cssls", "dockerls", "html", "jdtls", "jsonls", "tsserver" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "cssls", "dockerls", "html", "jdtls", "jsonls", "tsserver" },
         automatic_installation = true,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.dockerls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.dockerls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
