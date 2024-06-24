2717cf5022076d03567ce501d3c13231a911c5d2
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-24 10:28:22 -0300

    feat: add tab bar

---
+++ lua/plugins/tab-bar.lua
@@ -0,0 +1,21 @@
+return {
+	"romgrk/barbar.nvim",
+	dependencies = {
+		"lewis6991/gitsigns.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	init = function()
+		vim.g.barbar_auto_setup = false
+	end,
+	opts = {
+		animation = true,
+		insert_at_start = true,
+	},
+	config = function()
+		require("barbar").setup()
+		-- Move to previous/next
+		vim.keymap.set("n", "<Tab>", ":BufferPrevious<CR>", {})
+		vim.keymap.set("n", "<S-Tab>", ":BufferNext<CR>", {})
+		vim.keymap.set("n", "<leader>bc", ":BufferClose<CR>", {})
+	end,
+}
