d01860a2fc1b827c54dd2627145d26dfcc68cee6
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-06-28 09:12:10 -0300

    feat: add git config to neo-tree

--- lua/plugins/file-management.lua
+++ lua/plugins/file-management.lua
@@ -8,7 +8,54 @@ return {
 			"MunifTanjim/nui.nvim",
 			"3rd/image.nvim",
 		},
+		opts = {
+			filesystem = {
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
+			},
+		},
 		config = function()
+			require("neo-tree").setup({
+				enable_git_status = true,
+				default_component_configs = {
+					name = {
+						trailing_slash = false,
+						use_git_status_colors = true,
+						highlight = "NeoTreeFileName",
+					},
+					git_status = {
+						symbols = {
+							-- Change type
+							added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+							modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+							deleted = "✖", -- this can only be used in the git_status source
+							renamed = "󰁕", -- this can only be used in the git_status source
+							-- Status type
+							untracked = "",
+							ignored = "",
+							unstaged = "󰄱",
+							staged = "",
+							conflict = "",
+						},
+					},
+				},
+				git_status = {
+					window = {
+						position = "float",
+						mappings = {
+							["A"] = "git_add_all",
+							["gu"] = "git_unstage_file",
+							["ga"] = "git_add_file",
+							["gr"] = "git_revert_file",
+							["gc"] = "git_commit",
+							["gp"] = "git_push",
+							["gg"] = "git_commit_and_push",
+							["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+						},
+					},
+				},
+			})
+
 			vim.keymap.set("n", "f", ":Neotree filesystem reveal left<CR>", {})
 			vim.keymap.set("n", "<leader>ft", ":Neotree toggle<CR>", {})
 			vim.keymap.set("n", "<leader>fo", ":Neotree buffers<CR>", {})
@@ -23,7 +70,7 @@ return {
 			local oil = require("oil")
 			oil.setup()
 
-      vim.keymap.set("n", "-", oil.toggle_float, {})
+			vim.keymap.set("n", "-", oil.toggle_float, {})
 		end,
 	},
 }
