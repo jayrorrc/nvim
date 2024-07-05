ca17a1cf4ccc9f71e5952ae2acfbf922f33e000c
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2024-07-05 13:39:10 -0300

    feat: add keymap to clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,5 +6,9 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 
+vim.cmd('inoremap <C-v> <ESC>"+pa')
+vim.cmd('vnoremap <C-c> "+y')
+vim.cmd('vnoremap <C-d> "+d')
+
 vim.cmd("set list")
 vim.cmd("set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<")
